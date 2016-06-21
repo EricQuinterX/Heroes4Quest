74807cff5d6aeeb7880e440a78713e36cdd916f7
JorgeLuis <lescanojorgeluis@gmail.com>
2016-06-21 11:18:35 -0300

    Se suman test para validar los trabajos.

--- src/test/scala/TrabajoTest.scala
+++ src/test/scala/TrabajoTest.scala
@@ -3,8 +3,9 @@ import org.junit.Test
 
 class TrabajoTest {
 
+  //Test para validar trabajo de Guerreros
   @Test
-  def asignarTrabajoHeroe(): Unit = {
+  def heroeTrabajaDeGuerrero(): Unit = {
     val juan: Heroe = new Heroe(new Stats(100,50,30,10))
     juan.adquirirTrabajo(Guerrero)
     assertEquals(juan.atributos().fuerza, 65)
@@ -12,21 +13,71 @@ class TrabajoTest {
   }
 
   @Test
-  def heroeDejaTrabajo(): Unit = {
+  def heroeDejaDeTrabajarDeGuerrero(): Unit = {
     val gago = new Heroe(new Stats(10,10,10,10))
     gago.adquirirTrabajo(Guerrero) // (10,15,0,-10)
-    assertEquals(gago.atributos(), new Stats(20,25,10,1)) // (20,25,10,1)
+    assertEquals(gago.atributos(), new Stats(20,25,10,1))
     gago.dejarTrabajo()
-    assertEquals(gago.atributos(), new Stats(10,10,10,10)) // (10,10,10,10)
+    assertEquals(gago.atributos(), new Stats(10,10,10,10))
+  }
+
+  //Test para validar trabajo de Mago
+  @Test
+  def heroeTrabajaDeMago(): Unit = {
+    val harry = new Heroe(new Stats(20,20,20,20))
+    harry.adquirirTrabajo(Mago) // (0,-20,0,20)
+    assertEquals(harry.atributos(), new Stats(20,1,20,40))
   }
 
   @Test
-  def heroeCambiaDeTrabajoConAtributoNegativo(): Unit = {
-    val richi = new Heroe(new Stats(100,5,5,5))
-    richi.adquirirTrabajo(Mago) // (0,-20,0,20)
-    assertEquals(richi.atributos(), new Stats(100,1,5,25))
-    richi.adquirirTrabajo(Ladron) // (-5,0,10,0)
-    assertEquals(richi.atributos(), new Stats(95,5,15,5))
+  def heroeDejaDeTrabajarDeMago(): Unit = {
+    val harry = new Heroe(new Stats(10,10,10,10))
+    harry.adquirirTrabajo(Mago) // (0,-20,0,20)
+    assertEquals(harry.atributos(), new Stats(10,1,10,30))
+    harry.dejarTrabajo()
+    assertEquals(harry.atributos(), new Stats(10,10,10,10))
   }
+
+  //Test para validar trabajo de Ladron
+  //Stats (hp: Int, fuerza: Int, velocidad: Int, inteligencia: Int)
+  @Test
+  def heroeTrabajaDeLadron(): Unit = {
+    val harry = new Heroe(new Stats(20,20,20,20))
+    harry.adquirirTrabajo(Ladron) // (-5,0,+10,0)
+    assertEquals(harry.atributos(), new Stats(15,20,30,20))
+  }
+
+  @Test
+  def heroeDejaDeTrabajarDeLadron(): Unit = {
+    val harry = new Heroe(new Stats(1,10,10,10))
+    harry.adquirirTrabajo(Ladron) // (-5,0,+10,0)
+    assertEquals(harry.atributos(), new Stats(1,10,20,10))
+    harry.dejarTrabajo()
+    assertEquals(harry.atributos(), new Stats(1,10,10,10))
+  }
+
+   //Test para validar el cambio de trabajo, pasa de Ladron a Guerrero
+  @Test
+  def heroeTrabajaDeLadronLuegoGuerrero(): Unit = {
+    val harry = new Heroe(new Stats(20,20,20,20))
+    harry.adquirirTrabajo(Ladron)   // (-5,0,+10,0)
+    harry.adquirirTrabajo(Guerrero) // (10,15,0,-10)
+    assertEquals(harry.atributos(), new Stats(30,35,20,10))
+  }
+
+  //Stats (hp: Int, fuerza: Int, velocidad: Int, inteligencia: Int)
+  //Test para validar el cambio de trabajo, pasa de Ladron a Guerrero
+  // y por ultimo vuelve a stat original
+  @Test
+  def heroeTrabajaDeLadronLuegoGuerreroYLuegoRenuncia(): Unit = {
+    val harry = new Heroe(new Stats(20,20,20,20))
+    harry.adquirirTrabajo(Ladron)   // (-5,0,+10,0)
+    harry.adquirirTrabajo(Guerrero) // (10,15,0,-10)
+    harry.dejarTrabajo()
+    assertEquals(harry.atributos(),new Stats(20,20,20,20))
+  }
+
+
+
 
 }
