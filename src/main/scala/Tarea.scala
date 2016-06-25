52274516e2291ad8525e17e1c37e769630d7766a
joncolque <joncolque@gmail.com>
2016-06-25 16:10:52 -0300

    Brian hijo de Odersky 2

--- src/main/scala/Tarea.scala
+++ src/main/scala/Tarea.scala
@@ -17,15 +17,6 @@ class Tarea (name: String,
                                 TareaSuperada(unEquipo)
     }
   }
-
-
-
-  //Consideraciones:
-  //by jon: hago que el heroe conozca a su equipo pero se genera algo sucio como lo que sigue, no hago el test porque quiero validar con ustedes.
-  //atencion! : Cuando se crea el equipo, se le tiene que avisar a todos los heroes a que equipo pertenecen. Tambien quiero validar esto.
-  //Por mi parte, elegiria la opcion 2 que les comente en el houngouts queda menos sucio.
-  //No se como sacar el siguiente metodo afuera de la clase, sin tener que ponerlo como val en el Test!!!
-
 }
 
 
